2c5e081da6d7810b086127e671f50a99046fcd9e
brunoamadorsolucoes@gmail.com <brunoamadorsolucoes@gmail.com>
2023-01-11 18:41:46 -0300

    google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
 import Layout from '../components/Layout/Layout.jsx'
 import '../assets/styles/global.scss'
 import { RecoilRoot } from 'recoil'
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }) {
   return (
     <RecoilRoot>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-DMMDHSK6EP"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+        
+          gtag('config', 'G-DMMDHSK6EP');
+        `}
+      </Script>
       <Layout>
         <Component {...pageProps} />
       </Layout>
