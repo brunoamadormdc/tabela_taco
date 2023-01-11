3bf92da021f534dcde4959483cce26da84f5ad7a
brunoamadorsolucoes@gmail.com <brunoamadorsolucoes@gmail.com>
2023-01-11 11:26:40 -0300

    categories changes

---
+++ pages/api/getcategories.js
@@ -0,0 +1,15 @@
+import {taco_table} from '../../tables/taco_table'
+import {onlyUnique} from '../../helpers/index'
+
+export default function handler(req, res) {
+
+  let categorias = taco_table.map(val=> val.category)
+  let unique_categorias = categorias.filter(onlyUnique)
+
+  if(req == 'post') {
+    res.status(500).json({error:'Requisição não permitida'})
+  }
+  
+  res.status(200).json({ categorias: unique_categorias })
+
+}
