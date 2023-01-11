54f748db9b84b5bd4593e09d9242f6f83df413ff
brunoamadorsolucoes@gmail.com <brunoamadorsolucoes@gmail.com>
2023-01-11 19:17:14 -0300

    layout correction

--- components/Results/Results.jsx
+++ components/Results/Results.jsx
@@ -14,19 +14,19 @@ export default function Results() {
                         <div className={styles.title}><h2>{val.description}</h2></div>
 
                         <div className={styles.itens_container}>
-                            <div className={styles.itens}>{Math.round(val.energy_kcal)}/kcal</div>
-                            <div className={styles.itens}>{Math.round(val.protein_g)}g/proteín</div>
-                            <div className={styles.itens}>{Math.round(val.carbohydrate_g)}g/carbo</div>
-                            <div className={styles.itens}>{Math.round(val.fiber_g)}g/fibras</div>
+                            <div className={styles.itens}>Calorias: {Math.round(val.energy_kcal)}kcal</div>
+                            <div className={styles.itens}>Proteínas: {Math.round(val.protein_g)}g</div>
+                            <div className={styles.itens}>Carbo: {Math.round(val.carbohydrate_g)}g</div>
+                            <div className={styles.itens}>Fibras:{Math.round(val.fiber_g)}g</div>
 
-                            <div className={styles.itens}>{Math.round(val.lipid_g)}g/gordura</div>
-                            <div className={styles.itens}>{Math.round(val.lipid_g)}g/gord. saturada</div>
+                            <div className={styles.itens}>Gorduras: {Math.round(val.lipid_g)}g</div>
+                            <div className={styles.itens}>Gord.Saturada: {Math.round(val.lipid_g)}g</div>
                         </div>
 
 
                     </div>
 
-                </div>) : ''}
+                </div>) : <div className={styles.results_container}>Não foram encontrados resultados</div>}
             </div>
         </>
     )
