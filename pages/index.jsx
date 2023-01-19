47d6687907ba9230f1ee92832fcca7ba91b1f234
brunoamadorsolucoes@gmail.com <brunoamadorsolucoes@gmail.com>
2023-01-19 07:36:58 -0300

    meta tags insert

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,13 +1,36 @@
 import Results from "../components/Results/Results";
 import Search from "../components/Search/Search";
+import Head from "next/head";
+
+export async function getServerSideProps() {
+  const meta_title = 'Composição de Alimentos e Criação de Refeições'
+  const meta_description = 'Composição de Alimentos e Criação de Refeições - A melhor forma de criar refeições saudáveis e balanceadas.'
 
 
-export default function Home() {
+  return {
+    props: { meta_title, meta_description }, // will be passed to the page component as props
+  }
+}
+
+export default function Home(props) {
 
   return (
-    <> 
-      
+    <>
+      <Head>
+        <title>{props.meta_title}</title>
+        <meta name='description' content={`${props.meta_title}`} />
+        <meta property='og:title' content={`${props.meta_title}`} />
+        <meta property='og:description' content={`Compre nosso lanche ${props.meta_description}`} />
+        <meta
+          property='og:url'
+          content={`https://tabelataco.com.br`}
+        />
+        <meta property='og:type' content='website' />
+       
+      </Head>
+
       <Search></Search>
+      {props.propina}
       <Results></Results>
     </>
   )
--- pages/refeicoes.jsx
+++ pages/refeicoes.jsx
@@ -3,13 +3,35 @@ import {meals} from '../store/Meals'
 import styles from '../assets/styles/meals.module.scss'
 import CreateMeal from "../components/CreateMeal/CreateMeal"
 import ListMeals from "../components/ListMeals/ListMeals"
+import Head from "next/head"
 
-export default function Meals () {
+export async function getServerSideProps() {
+    const meta_title = 'Criador de refeições'
+    const meta_description = 'Composição de Alimentos e Criação de Refeições para melhor aproveitamento do APP.'
+
+    return { 
+        props: { meta_title, meta_description }, // will be passed to the page component as props
+    }
+}
+
+export default function Meals (props) {
 
     const [vmeals,getVMeals] = useRecoilState(meals)
 
     return (
         <>
+        <Head>
+        <title>{props.meta_title}</title>
+        <meta name='description' content={`${props.meta_title}`} />
+        <meta property='og:title' content={`${props.meta_title}`} />
+        <meta property='og:description' content={`Compre nosso lanche ${props.meta_description}`} />
+        <meta
+          property='og:url'
+          content={`https://tabelataco.com.br`}
+        />
+        <meta property='og:type' content='website' />
+       
+      </Head>
             <div className={styles.meals}>
                 <h2>Crie e visualize suas refeições</h2>
                 <div className={styles.meals_container}>
